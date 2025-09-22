5dbdad69a527a5237bd2aab9e7bdf4b152696400
Avarice/RojTheGod <77698878+AvariceBrah@users.noreply.github.com>
2025-09-22 06:03:09 -0500

    Update RevenantImp.ts

--- packages/oldschooljs/src/simulation/monsters/low/n-s/RevenantImp.ts
+++ packages/oldschooljs/src/simulation/monsters/low/n-s/RevenantImp.ts
@@ -35,7 +35,7 @@ export const RevenantImpTable = new LootTable()
 	/* Other */
 	.add('Coins', [1, 51], 3145)
 	.add('Revenant cave teleport', 1, 7)
-	.add('Super restore(4)', [3, 5], 4)
+	.add('Blighted Super restore(4)', [3, 5], 4)
 	.add('Dragonstone bolt tips', [40, 70], 4)
 	.add('Onyx bolt tips', [5, 10], 4)
 	.add('Law rune', [80, 120], 3)
